26f32cd4c5096d6ba304f5e471837eee4dc3429f
jcbrady419 <70285750+jcbrady419@users.noreply.github.com>
2021-02-22 13:45:20 -0500

    Timer Added

--- GP_Project2/Assets/Scripts/PlayerMovement.cs
+++ GP_Project2/Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float currentTime = 0f;
+    public float startingTime = 12f;
+    public Text timer;
+
+    void Start()
+    {
+        currentTime = startingTime;
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,5 +25,13 @@ public class PlayerMovement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
+
+         currentTime -= 1 * Time.deltaTime;
+        timer.text = currentTime.ToString("0");
+        if (currentTime <= 0)
+        {
+            currentTime = 0f;
+        }
+       
     }
 }
